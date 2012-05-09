be749088efc1b667420e901c26115a34238a435f
zan0701 <zankhana.pandya@narolainfotech.com>
2012-05-09 12:37:31 +0530

    FFF

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,8 +14,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //testing data...
-        //testing data 1...
-        //testing data 2...
+        
     }
 }
