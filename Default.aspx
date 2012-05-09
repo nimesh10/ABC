63e25de61f45875268cebbe14b965b2150f74e5e
zan0701 <zankhana.pandya@narolainfotech.com>
2012-05-09 14:41:37 +0530

    Ten Commit

--- Default.aspx
+++ Default.aspx
@@ -9,7 +9,7 @@
 <body>
     <form id="form1" runat="server">
     <div>
-   <%--Nimesh--%>
+        <%--Default.aspx--%>
     </div>
     </form>
 </body>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,6 +14,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //Hello world
+        //Default.aspx.cs
     }
 }
